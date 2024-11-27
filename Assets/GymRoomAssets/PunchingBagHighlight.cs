ebd0bde15d15e1f545089a460c37ab22917e295b
Calvin Hunter <70008107+CalvinHunter03@users.noreply.github.com>
2024-11-26 18:34:09 -0800

    punching bag dies

--- Assets/GymRoomAssets/PunchingBagHighlight.cs
+++ Assets/GymRoomAssets/PunchingBagHighlight.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PunchingBagHighlight : MonoBehaviour
@@ -8,6 +9,10 @@ public class PunchingBagHighlight : MonoBehaviour
     public GameObject Obj;
     private Renderer renderer;
     private Color initialColor;
+
+    public Camera camera;
+
+    [SerializeField] private float punchBagHealth = 100f;
 
     void Start()
     {
@@ -18,12 +23,35 @@ public class PunchingBagHighlight : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetMouseButtonDown(0)){
+            Ray ray = camera.ScreenPointToRay(Input.mousePosition);
 
+            if(Physics.Raycast(ray, out RaycastHit hitInfo)){
+                if(hitInfo.collider.gameObject.tag == "punchBag"){
+                    
+                    punchBagHealth -= 10f;
+                    Debug.Log("Health: " + punchBagHealth);
+                    
+                }
+            }
+        }
+
+        punchBagHealth += 0.2f;
+
+        if(punchBagHealth > 100f){
+            punchBagHealth = 100f;
+            Debug.Log("Punching Bag Fully Healed");
+        }
+        if(punchBagHealth <= 0){
+            Obj.SetActive(false);
+        }
+
+        
     }
 
     private void OnMouseEnter()
     {
-        renderer.material.color = Color.red;
+        renderer.material.color = Color.red + initialColor;
     }
 
     private void OnMouseExit()
