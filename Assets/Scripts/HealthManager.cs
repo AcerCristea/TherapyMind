78717c18bb5489c8c78fd28635e620954d20d0df
AcerCristea <acercristea@gmail.com>
2024-12-05 06:50:32 -0800

    InsanityBarIncreasesUI

--- Assets/Scripts/HealthManager.cs
+++ Assets/Scripts/HealthManager.cs
@@ -3,28 +3,51 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class HealthManager : MonoBehaviour
+public class InsanityManager : MonoBehaviour
 {
-    public Image healthBar;
-    public float healthAmount = 100f;
+    public Image insanityBar; // Reference to the UI Image for the slider bar
+    public float maxInsanity = 100f; // Maximum insanity level
+    public float insanityAmount = 0f; // Current insanity level
 
-    // Update is called once per frame
+
+    private GameManager gameManager;
+
+
+
+    void Start()
+    {
+        // Find the GameManager in the scene
+        gameManager = GameManager.instance;
+
+        if (gameManager == null)
+        {
+            Debug.LogError("GameManager not found in the scene!");
+        }
+    }
+
     void Update()
     {
-        
+        if (gameManager != null)
+        {
+            // Update insanity amount from GameManager
+            insanityAmount = gameManager.insanityMeter;
+
+            // Update the insanity bar fill
+            insanityBar.fillAmount = insanityAmount / maxInsanity;
+        }
     }
 
-    public void TakeDamage(float damage)
-    {
-        healthAmount -= damage;
-        healthBar.fillAmount = healthAmount / 100;
-    }
+    //public void TakeDamage(float damage)
+    //{
+    //    healthAmount -= damage;
+    //    healthBar.fillAmount = healthAmount / 100;
+    //}
 
-    public void Heal(float healingAmount)
-    {
-        healthAmount += healingAmount;
-        healthAmount = Mathf.Clamp(healthAmount, 0, 100);
+    //public void Heal(float healingAmount)
+    //{
+    //    healthAmount += healingAmount;
+    //    healthAmount = Mathf.Clamp(healthAmount, 0, 100);
 
-        healthBar.fillAmount = healthAmount / 100f;
-    }
+    //    healthBar.fillAmount = healthAmount / 100f;
+    //}
 }
